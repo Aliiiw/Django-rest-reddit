36123c0878188760bc34919f60f046392d443d8b
Ali Rahimi <1aliirahimi1@gmail.com>
2022-07-31 21:27:42 +0430

    add permissions and create api

--- reddit/posts/serializers.py
+++ reddit/posts/serializers.py
@@ -1,9 +1,13 @@
 from dataclasses import fields
+from numpy import source
 from rest_framework import serializers
 from .models import Post
 
 
 class PostSerializers(serializers.ModelSerializer):
+    poster = serializers.ReadOnlyField(source='poster.username')
+    poster_id = serializers.ReadOnlyField(source='poster.id')
+
     class Meta:
         model = Post
-        fields = ['id', 'title', 'url', 'poster', 'created_at']
+        fields = ['id', 'title', 'url', 'poster', 'poster_id', 'created_at']
