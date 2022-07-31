461146f0014e36e5ab022bd1dc482843e53212c8
Ali Rahimi <1aliirahimi1@gmail.com>
2022-07-31 21:03:32 +0430

    create models and views and serializers

---
+++ Rest_Reddit/posts/serializers.py
@@ -0,0 +1,9 @@
+from dataclasses import fields
+from rest_framework import serializers
+from .models import Post
+
+
+class PostSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'url', 'poster', 'created_at']
